1741c8c86327958cd7ea63db9031fb0a5315302e
Khang Tran <ktra@email.unc.edu>
2023-08-10 17:55:42 -0400

    add data

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,16 @@
 import dotenv from "dotenv";
 import express from "express";
+import fs from "fs/promises";
 
 const app = express();
 dotenv.config();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
-app.get("/", (req, res) => {
-	res.send("Hello world!");
+app.get("/", async (req, res) => {
+	const data = await fs.readFile("./data/set_classes.json", "utf8");
+	const body = JSON.parse(data);
+	res.send(body);
 });
 
 // start the Express server
